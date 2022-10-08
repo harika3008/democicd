00951ccee0330ba2e256346735a152b169c25ac9
harika3008 <harikap@gmail.com>
2022-10-08 07:51:28 +0530

    first cicd code committed

---
+++ src/test/java/com/cicd/test1.java
@@ -0,0 +1,36 @@
+package com.cicd;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class test1 {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\HARIKA\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		//driver.get("http://the-internet.herokuapp.com/");
+		driver.get("https://www.amazon.in/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=inflex&mobileBrowserWeblabTreatment=C&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&prevRID=DHF8EG92T7KT64K0SCJK&openid.assoc_handle=inflex&openid.mode=checkid_setup&desktopBrowserWeblabTreatment=C&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("Title 1= "+ driver.getTitle());
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+	
+}
